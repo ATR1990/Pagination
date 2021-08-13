1a025102d306f23985360190abac1f90be2400fc
ATR1990 <t.altynbekov@sapasoft.kz>
2021-08-13 17:07:00 +0600

    Initial commit 1

---
+++ src/app/users.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private url = 'https://api.github.com/users/gaearon/followers?per_page=100';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.url);
+  }
+}
